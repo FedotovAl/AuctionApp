a4195c910d7bcdecc6f531e942e5b7ff44930abf
Aleksei Fedotov1 <aleksei_fedotov1@epam.com>
2021-03-19 15:30:41 +0400

    removed unnecessary imports

--- src/main/java/com/example/mp/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/com/example/mp/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,5 @@
 package com.example.mp.config;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
--- src/main/java/com/example/mp/config/SecurityInitializer.java
+++ src/main/java/com/example/mp/config/SecurityInitializer.java
@@ -1,6 +1,5 @@
 package com.example.mp.config;
 
-import org.springframework.core.annotation.Order;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
